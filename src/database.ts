6eb896dacc025581189b4bf3f7fcfd83aef3f04b
Lucas Zluhan <lszluhan@hotmail.com>
2021-11-26 20:47:05 -0300

    deletei usuario teste

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,4 @@ import { User } from './classes';
 
 let users: User[] = [];
 
-let lucas: User = new User('lucas', '123');
-lucas.notes = [{ noteNumb: 0, detail: 'lalala', description: 'lalalala' }];
-users.push(lucas);
-
 export default users;
